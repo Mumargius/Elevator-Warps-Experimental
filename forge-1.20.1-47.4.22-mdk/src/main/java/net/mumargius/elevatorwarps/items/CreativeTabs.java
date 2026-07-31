201cbb372c292d4b815b99d65ff7f485d978ad55
Mumargius <mumargius@protonmail.com>
2026-07-30 22:11:57 -0400

    Understood crafting recipes

--- forge-1.20.1-47.4.22-mdk/src/main/java/net/mumargius/elevatorwarps/items/CreativeTabs.java
+++ forge-1.20.1-47.4.22-mdk/src/main/java/net/mumargius/elevatorwarps/items/CreativeTabs.java
@@ -14,10 +14,10 @@ public class CreativeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS=
         DeferredRegister.create(Registries.CREATIVE_MODE_TAB, ElevatorWarps.MODID);
 
-        public static final RegistryObject<CreativeModeTab> CREATIVE_TAB = 
-        CREATIVE_MODE_TABS.register("other_tab", 
+        public static final RegistryObject<CreativeModeTab> OTHERSTUFF = 
+        CREATIVE_MODE_TABS.register("otherstuff", 
             () -> CreativeModeTab.builder().icon(()->new ItemStack(WarpItems.TESTBEAR.get()))
-                .title(Component.translatable("creativetab.elevatorwarps_tab"))
+                .title(Component.translatable("creativetab.otherstuff_tab"))
                 .displayItems((pParameters,pOutput)->{
                     pOutput.accept(WarpItems.TESTBEAR.get());
                     pOutput.accept(WarpItems.WARP_WHISTLE.get());
@@ -28,6 +28,19 @@ public class CreativeTabs {
 
                 }).build());
 
+        public static final RegistryObject<CreativeModeTab> OTHEROTHERSTUFF = 
+        CREATIVE_MODE_TABS.register("otherotherstuff", 
+            () -> CreativeModeTab.builder().icon(()->new ItemStack(WarpItems.TESTBEAR.get()))
+                .title(Component.translatable("creativetab.otherotherstuff_tab"))
+                .displayItems((pParameters,pOutput)->{
+                    pOutput.accept(WarpItems.TESTBEAR.get());
+                    //pOutput.accept(WarpItems.WARP_WHISTLE.get());
+                   
+                    pOutput.accept(WarpBlocks.BEARBLOCK.get());
+                    //pOutput.accept(WarpBlocks.OTHERBEARBLOCK.get());
+
+
+                }).build());
         public static void register(IEventBus eventBus){
             CREATIVE_MODE_TABS.register(eventBus);
         }
